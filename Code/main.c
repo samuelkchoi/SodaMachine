2e5b53f87b7f5faff05d8ed19b4c1477a912dd89
Samuel Choi <choi.k.samuel@gmail.com>
2017-05-31 16:49:00 -0700

    Soda Machine

--- Code/main.c
+++ Code/main.c
@@ -19,9 +19,7 @@ void ADC_init() {
 	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
 	// ADEN: setting this bit enables analog-to-digital conversion.
 	// ADSC: setting this bit starts the first conversion.
-	// ADATE: setting this bit enables auto-triggering. Since we are
-	// in Free Running Mode, a new conversion will trigger whenever
-	// the previous conversion completes.
+	// ADATE: setting this bit enables auto-triggering. 
 }
 
 void soda_tick()
